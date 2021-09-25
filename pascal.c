398ea32a2f38de3eca97abd11a034fb17a05e1d6
Akash <akashdeshpande118@gmail.com>
2021-09-25 22:21:58 +0530

    Switched from static array to dynamic array, demo init and print.

--- pascal.c
+++ pascal.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
@@ -6,23 +7,54 @@ int main(void)
     printf("Enter no. of lines: ");
     scanf("%d", &no_of_lines);
 
-    int even_array[1] = {1};
-    int odd_array[1] = {1};
+    int *odd_array = NULL;
+    int *even_array = NULL;
 
     for (int i = 0; i < no_of_lines; i++)
     {
         if (i % 2 == 0) // even i, but odd row
         {
-            printf("\nodd row\n");
+            // realloc odd array
+            odd_array = (int *)realloc(odd_array, sizeof(int) * (i + 1));
+
+            // business logic
+            // init array for demo
+            for (int k = 0; k < (i + 1); k++)
+            {
+                odd_array[k] = 1;
+            }
+
+            // print array
+            for (int j = 0; j < i + 1; j++)
+            {
+                printf("%d", odd_array[j]);
+            }
         }
         else // odd i, but even row
         {
-            printf("\neven row\n");
-        }
-        for (int j = 0; j < i + 1; j++)
-        {
-            printf("%d", 1);
+            // realloc even array
+            even_array = (int *)realloc(even_array, sizeof(int) * (i + 1));
+
+            // business logic
+            // init array for demo
+            for (int k = 0; k < (i + 1); k++)
+            {
+                even_array[k] = 1;
+            }
+
+            // print array
+            for (int j = 0; j < i + 1; j++)
+            {
+                printf("%d", even_array[j]);
+            }
         }
         printf("\n");
     }
 }
+
+/*
+Program comments
+
+malloc is same as realloc(null, ..)
+
+*/
